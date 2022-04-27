be9dca86879ccf78d39c3f41a27768b659ae2837
pandalaf <pandalaf@student.42wolfsburg.de>
2022-04-27 23:36:34 +0200

    made test for library, first version, doesn't compile

---
+++ test.c
@@ -0,0 +1,15 @@
+/*
+Code to test the library's functions
+*/
+
+#include "libft.h"
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\n----|----|----|-Test|----|----|----\n");
+	printf("Can the functions be called?\n");
+	printf("Function: ft_isalpha       | Out: %d", ft_isalpha('a'));
+	printf("Function: ft_isdigit       | Out: %d", ft_isdigit('a'));
+	return (0);
+}
