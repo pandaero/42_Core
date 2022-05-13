b967858a2685e80f65124767ed9825f44a15d2e1
pandalaf <pandalaf@student.42wolfsburg.de>
2022-05-13 15:39:52 +0200

    Added working ft_isalnum

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 19:26:47 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/05/11 17:28:25 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/05/13 15:38:20 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,7 @@
 int	ft_isalpha(char ch);
 //Function determines whether input character is a digit.
 int	ft_isdigit(char ch);
+//Function determinse whether input character is alphanumeric.
+int	ft_isalnum(char ch);
 
 #endif
