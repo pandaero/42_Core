c4a07c973252343264c4f30db389f0f75ee111a5
pandalaf <pandalaf@student.42wolfsburg.de>
2022-05-13 18:07:46 +0200

    updated test for library

--- test.c
+++ test.c
@@ -11,5 +11,8 @@ int main(void)
 	printf("Can the functions be called?\n");
 	printf("Function: ft_isalpha       | Out: %d", ft_isalpha('a'));
 	printf("Function: ft_isdigit       | Out: %d", ft_isdigit('a'));
+	printf("Function: ft_isalnum       | Out: %d", ft_isalnum('a'));
+	printf("Function: ft_isascii       | Out: %d", ft_isascii('a'));
+	printf("Function: ft_isprint       | Out: %d", ft_isprint('a'));
 	return (0);
 }
