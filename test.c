7ac67d7316a26733dc3826ac4fc349753a43c721
pandalaf <pandalaf@student.42wolfsburg.de>
2022-05-15 21:55:13 +0200

    notes and test

--- test.c
+++ test.c
@@ -8,11 +8,12 @@ Code to test the library's functions
 int main(void)
 {
 	printf("\n----|----|----|-Test|----|----|----\n");
-	printf("Can the functions be called?\n");
-	printf("Function: ft_isalpha       | Out: %d", ft_isalpha('a'));
-	printf("Function: ft_isdigit       | Out: %d", ft_isdigit('a'));
-	printf("Function: ft_isalnum       | Out: %d", ft_isalnum('a'));
-	printf("Function: ft_isascii       | Out: %d", ft_isascii('a'));
-	printf("Function: ft_isprint       | Out: %d", ft_isprint('a'));
+	printf("Can the functions be called? (Output unimportant)\n");
+	printf("Function: ft_isalpha       | Out: %d\n", ft_isalpha('a'));
+	printf("Function: ft_isdigit       | Out: %d\n", ft_isdigit('a'));
+	printf("Function: ft_isalnum       | Out: %d\n", ft_isalnum('a'));
+	printf("Function: ft_isascii       | Out: %d\n", ft_isascii('a'));
+	printf("Function: ft_isprint       | Out: %d\n", ft_isprint('a'));
+	printf("Function: ft_strlen        | Out: %d\n", ft_strlen("Hello"));
 	return (0);
 }
