8cd2df6cad4a635e8074292c3412383a4d0c8a11
pandalaf <pandalaf@student.42wolfsburg.de>
2022-05-15 21:10:58 +0200

    Added functions memset and bzero

---
+++ ft_memset.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/11 17:13:03 by pandalaf          #+#    #+#             */
+/*   Updated: 2022/05/15 21:05:18 by pandalaf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Function writes a value to a determined length of bytes in a string's memory.
+Returns the first argument, the string's first character.
+Turns the value to unsigned char, then writes it to each location within length.
+*/
+#include <stddef.h>
+
+void	*ft_memset(void *bstr, int ch, size_t len)
+{
+	size_t			i;
+	unsigned char	uch;
+	unsigned char	*mptr;
+
+	i = 0;
+	uch = (unsigned char) ch;
+	mptr = (unsigned char *) bstr;
+	while (i < len)
+	{
+		mptr[i] = uch;
+		i++;
+	}
+	return (bstr);
+}
+
+/* Test
+#include <stdio.h>
+
+int	main(void)
+{
+	char	str[] = "Hello";
+	char	*ret;
+
+	printf("Before:  %s\n", str);
+	printf("Address: %x\n", *str);
+	ret = ft_memset(str, 'c', 3);
+	printf("After:   %s\n", str);
+	printf("Address: %x", *ret);
+	return (0);
+}
+//*/
--- libft.h
+++ libft.h
@@ -6,12 +6,13 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 19:26:47 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/05/15 15:31:46 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/05/15 21:10:01 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+# include <stddef.h>
 
 //Function determines whether input character is alphabetic.
 int				ft_isalpha(char ch);
@@ -25,5 +26,9 @@ int				ft_isascii(char ch);
 int				ft_isprint(char ch);
 //Function determines length of the string.
 unsigned int	ft_strlen(const char *str);
+//Function writes a value to a determined length of bytes in memory.
+void			ft_memset(void *byte, int ch, size_t len);
+//Function sets determined length of memory in a string to zero.
+void			ft_bzero(void *str, size_t len);
 
 #endif
