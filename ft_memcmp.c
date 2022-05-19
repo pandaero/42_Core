d762f6b9101f93f25ffc1100154a3c688c4b9645
pandalaf <pandalaf@student.42wolfsburg.de>
2022-05-19 19:46:00 +0200

    test written for memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,14 +6,14 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 17:13:03 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/05/19 16:53:02 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/05/19 19:41:16 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 Function compares two byte strings for equality.
 Returns 0 if the strings are identical or the difference between first non-same.
-rocess
+Runs through strings and returns difference if the location differs. 0 when not.
 */
 #include <stddef.h>
 
@@ -37,9 +37,22 @@ int	ft_memcmp(const void *str1, const void *str2, size_t n)
 
 /* Test
 #include <stdio.h>
+#include <string.h>
 
 int	main(void)
 {
+	char s[] = {-128, 0, 127, 0};
+	char sCpy[] = {-128, 0, 127, 0};
+	char s2[] = {0, 0, 127, 0};
+	char s3[] = {0, 0, 42, 0};
+
+	printf("----|----|----|-Test|----|----|----|\n");
+	printf("Library: 1-%d, 2-%d, 3-%d, 4-%d, 5-%d", memcmp(s, sCpy, 4),
+			memcmp(s, s2, 0), memcmp(s, s2, 1), memcmp(s2, s, 1),
+			memcmp(s2, s3, 4));
+	printf("Library: 1-%d, 2-%d, 3-%d, 4-%d, 5-%d", ft_memcmp(s, sCpy, 4),
+			ft_memcmp(s, s2, 0), ft_memcmp(s, s2, 1), ft_memcmp(s2, s, 1),
+			ft_memcmp(s2, s3, 4));
 	return (0);
 }
 //*/
