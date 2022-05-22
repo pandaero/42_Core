e022ec1682d6e026226a1a47b952498b916339f2
pandalaf <pandalaf@student.42wolfsburg.de>
2022-05-22 21:01:37 +0200

    added template file to repo

---
+++ ft_template.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_template.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/11 17:13:03 by pandalaf          #+#    #+#             */
+/*   Updated: 2022/05/20 13:08:12 by pandalaf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Description of function
+Overview
+Output
+Process
+*/
+
+int		functionint(void)
+{
+	return (0);
+}
+void	functionvoid(void)
+{
+
+}
+
+/* Test | gcc -Wall -Werror -Wextra ft_template.c && ./a.out
+#include <stdio.h>
+
+int	main(void)
+{
+	return (0);
+}
+//*/
