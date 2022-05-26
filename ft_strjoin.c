0dfa756712e193d8c72b6c97cbedbc2bf08634e5
pandalaf <pandalaf@student.42wolfsburg.de>
2022-05-26 17:12:14 +0200

    added test for strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,15 +6,14 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 17:13:03 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/05/26 17:07:42 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/05/26 17:12:00 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-Description of function
-Overview
-Output
-Process
+Function concatenates two character strings, creates a new char string.
+A pointer to the new character string.
+Allocates memory for joined string, then copies from first, then second and nul.
 */
 #include <stdlib.h>
 #include "libft.h"
@@ -44,11 +43,15 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	return ((char *) ptr);
 }
 
-/* Test | gcc -Wall -Werror -Wextra ft_template.c && ./a.out
+/* Test | gcc -Wall -Werror -Wextra ft_strjoin.c ft_strlen.c && ./a.out
 #include <stdio.h>
 
 int	main(void)
 {
+	char s1[] = "You";
+	char s2[] = " and me";
+
+	printf("Test: s1-%s, s2-%s, Out-%s", s1, s2, ft_strjoin(s1, s2));
 	return (0);
 }
 //*/
