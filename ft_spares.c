ba126684065f0384578b0b29f06939f5f09076c4
pandalaf <pandalaf@student.42wolfsburg.de>
2022-07-12 11:56:32 -0400

    added more spares

--- ft_spares.c
+++ ft_spares.c
@@ -16,6 +16,17 @@ Several Outputs
 Several Methods
 */
 
+//Function determines whether character is a spacing character.
+static int	ft_isspace(int ch)
+{
+	if (ch == ' ')
+		return (1);
+	else if (ch == '\f' || ch == '\n' || ch == '\r' || ch == '\t' || ch == '\v')
+		return (1);
+	return (0);
+}
+
+
 //Function finds first instance of a character within a string, returns index.
 static unsigned int	ft_strfichr(const char *str, int ch)
 {
@@ -49,6 +60,73 @@ static unsigned int	ft_strlichr(const char *str, int ch)
 		lc = i;
 	return (lc);
 }
+
+//Function finds number of "words" in string between a set delimiter character.
+static unsigned int	ft_nwords(const char *str, unsigned char ch)
+{
+	unsigned int	i;
+	unsigned int	cnt;
+
+	i = 0;
+	cnt = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == ch)
+		{
+			if (str[i + 1] != ch && str[i + 1 != '\0'])
+				cnt++;
+			i++;
+		}
+		else
+		{
+			if (cnt == 0)
+				cnt++;
+			i++;
+		}
+	}
+	return (cnt);
+}
+
+//Function determines the sizes of "words" in string between set delimiter char.
+//Function returns an array with each words' size.
+static unsigned int	*ft_szwords(const char *str, unsigned int nwords, unsigned char ch)
+{
+	unsigned int	*ptr;
+	unsigned int	i;
+	unsigned int	sz;
+	unsigned int	cnt;
+
+	ptr = malloc(sizeof(unsigned int) * nwords);
+	if(!ptr)
+		return ((unsigned int *) 0);
+	i = 0;
+	sz = 0;
+	cnt = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == ch)
+		{
+			if (str[i + 1] != ch && str[i + 1] != '\0')
+				cnt++;
+			i++;
+		}
+		else
+		{
+			if (cnt == 0)
+				cnt++;
+			sz++;
+			if (str[i + 1] == ch && str[i + 1] != '\0')
+			{
+				cnt++;
+				ptr[cnt] = sz;
+				sz = 0;
+			}
+			i++;
+		} 
+	}	
+	return (ptr);
+}
+
 
 /* Test | gcc -Wall -Werror -Wextra ft_template.c && ./a.out
 #include <stdio.h>
