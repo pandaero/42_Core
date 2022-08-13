cc395aa3ca032288fb3ebfea5aa28166e95302f9
pandalaf <pandalaf@student.42.wolfsburg.de>
2022-08-13 18:05:20 -0400

    added provision for relative locations of src and dst

--- ft_memmove.c
+++ ft_memmove.c
@@ -13,9 +13,10 @@
 /*
 Function moves n bytes (char 8-bits) from source string to destination string.
 Returns pointer to destination string. Destination string has been written to.
-Makes pointers to characters (they store 1 byte), and writes to destination.
+Makes pointers to characters (they store 1 byte), writes to temp, then to dest.
 */
 #include <stddef.h>
+#include "libft.h"
 
 void	*ft_memmove(void *dst, const void *src, size_t n)
 {
@@ -23,18 +24,25 @@ void	*ft_memmove(void *dst, const void *src, size_t n)
 	unsigned char	*pdst;
 	unsigned char	*psrc;
 
-	i = 0;
+	if (!src || !dst || n == 0)
+		return (dst);
 	psrc = (unsigned char *) src;
 	pdst = (unsigned char *) dst;
-	while (n > i)
+	if (dst < src)
+		dst = ft_memcpy(dst, src, n);
+	else
 	{
-		pdst[n - i - 1] = psrc[n - i - 1];
-		i++;
+		i = 0;
+		while (n > i)
+		{
+			pdst[n - i - 1] = psrc[n - i - 1];
+			i++;
+		}
 	}
 	return ((void *) dst);
 }
 
-/* Test | gcc -Wall -Werror -Wextra ft_memmove.c && ./a.out
+/* Test | gcc -Wall -Werror -Wextra ft_memmove.c ft_memcpy.c && ./a.out
 #include <stdio.h>
 #include <string.h>
 
@@ -42,14 +50,15 @@ int	main(void)
 {
 	char	dst[] = "500.000";
 	char	dstfn[] = "500.000";
-	char	src[] = "two";
+	char	src[] = "two3452";
 	char	dstcpy[] = "500.000";
+	size_t	n = 0;
 
 	printf("----|-Test|----\n");
 	printf("Mine|-- Before: %s, Src: %s, After: %s\n",
-			dstcpy, src, (char *) ft_memmove(dst, src,3));
+			dstcpy, src, (char *) ft_memmove(dst, src,n));
 	printf("Lib-|-- Before: %s, Src: %s, After: %s\n",
-			dstcpy, src, (char *) memmove(dstfn, src, 3));
+			dstcpy, src, (char *) memmove(dstfn, src, n));
 	return (0);
 }
 //*/
