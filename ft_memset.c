3f714b4f5029daef4a30c6bfc8466528e494cacc
pandalaf <pandalaf@student.42.wolfsburg.de>
2022-08-13 18:32:33 -0400

    removed extra steps that produced casting errors

--- ft_memset.c
+++ ft_memset.c
@@ -17,36 +17,40 @@ Turns the value to unsigned char, then writes it to each location within length.
 */
 #include <stddef.h>
 
-void	*ft_memset(void *bstr, int ch, size_t len)
+void	*ft_memset(void *str, int ch, size_t n)
 {
 	size_t			i;
-	unsigned char	uch;
 	unsigned char	*mptr;
 
+	mptr = (unsigned char *) str;
 	i = 0;
-	uch = (unsigned char) ch;
-	mptr = (unsigned char *) bstr;
-	while (i < len)
+	while (n > i)
 	{
-		mptr[i] = uch;
+		mptr[i] = ch;
 		i++;
 	}
-	return (bstr);
+	return (str);
 }
 
 /* Test | gcc -Wall -Werror -Wextra ft_memset.c && ./a.out
 #include <stdio.h>
+#include <string.h>
 
 int	main(void)
 {
 	char	str[] = "Hello";
+	char	strcp[] = "Hello";
 	char	*ret;
+	char	*ret2;
 
 	printf("Before:  %s\n", str);
 	printf("Address: %x\n", *str);
-	ret = ft_memset(str, 'c', 3);
-	printf("After:   %s\n", str);
-	printf("Address: %x", *ret);
+	ret = ft_memset(str, 'x', 2);
+	printf("After:     %s\n", str);
+	ret2 = memset(strcp, 'x', 2);
+	printf("Lib After: %s\n", strcp);
+	printf("Address:     %x\n", *ret);
+	printf("Lib Address: %x\n", *ret2);
 	return (0);
 }
 //*/
